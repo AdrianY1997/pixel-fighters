63bd03fa0402d7767f2a469a1935e04269939e51
Luis Carlos Medina <luiscarlosmedina481@gmail.com>
2023-09-26 16:46:06 -0500

    chore: add option login dashboard

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,19 @@
 import "../../public/css/tailwind.compiled.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function RootLayout({ children }) {
   return (
     <>
       <html>
-        <body className="bg-[whitesmoke]">
-          <header>Header</header>
-          <main>{children}</main>
-          <footer>Footer</footer>
+        <body>
+          <header>
+            <Navbar />
+          </header>
+          <main className="bg-[whitesmoke]">{children}</main>
+          <footer className="bg-white w-full shadow dark:bg-gray-900">
+            <Footer />
+          </footer>
         </body>
       </html>
     </>
---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 .Senasoft - Desarrollo integral.</span>
+    </div>
+  )
+}
