dfee26f9d5802903bd4067ede33c1139f8bc0bf6
adriany1997 <yasnoadriandatos@gmail.com>
2023-09-27 09:04:48 -0500

    update: updating references & apply auth

--- src/app/(auth)/registro/page.jsx
+++ src/app/(auth)/registro/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import InputGroup from "@/components/form/inputGroup";
 import Link from "next/link";
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import checkUser from "@/services/database/checkUser";
+import NavbarLinks from "./navbarLinks";
 
-export default async function Navbar() {
-  const { isLogged, isAdmin } = checkUser();
+export default function Navbar() {
   return (
     <div>
       <nav className=" shadow-sm bg-white fixed w-full">
@@ -11,25 +10,14 @@ export default async function Navbar() {
           <div className="flex items-center h-15 w-full">
             <div className="flex items-center  mx-5 justify-between w-full ">
               <div className="flex justify-center items-center flex-shrink-0 ">
-                <h1 className="font-bold text-xl cursor-pointer">
-                  Pixel project
-                </h1>
+                <Link href={"/inicio"}>
+                  <h1 className="font-bold text-xl cursor-pointer">
+                    Pixel project
+                  </h1>
+                </Link>
               </div>
               <div className="hidden md:block">
-                <div className=" flex items-baseline justify-end space-x-4">
-                  <Link
-                    href="/registro"
-                    className="cursor-pointer text-black hover:text-blue-600 px-4 py-2 rounded-md"
-                  >
-                    Registro
-                  </Link>
-                  <Link
-                    href="/ingreso"
-                    className="cursor-pointer bg-blue-500 text-white px-4 py-1 rounded-md"
-                  >
-                    Acceder
-                  </Link>
-                </div>
+                <NavbarLinks />
               </div>
             </div>
           </div>
