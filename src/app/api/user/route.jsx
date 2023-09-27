d7768608bfc237863ea99a58a7db56315beab964
Luis Carlos Medina <luiscarlosmedina481@gmail.com>
2023-09-27 16:21:53 -0500

    chore: add  funcion registrar

--- src/app/api/user/route.jsx
+++ src/app/api/user/route.jsx
@@ -5,5 +5,17 @@ export async function GET() {
 
   return Response.json(data.length ? true : false);
 }
+export async function POST(request) {
+  // const data = await prisma.user.findMany();
+  const datarec = await request.json()
+  const user = await prisma.user.create({data:{
+    user_email:datarec.user_email,
+    user_name:datarec.user_name,
+    user_password:datarec.user_password,
+    user_level:datarec.user_level,
+    user_role:"1",
+  }})
+  console.log(datarec);
 
-export async function POST(request) {}
+  return Response.json({});
+}
--- src/components/dojo.jsx
+++ src/components/dojo.jsx
@@ -4,19 +4,26 @@ import React from "react";
 export default function Dojo({ list }) {
   return (
     <div>
+      {/* Título de la sección */}
       <h1 className="text-blue-500 text-xl font-bold m-1 mb-2">Tus dojos</h1>
       <div>
+        {/* Comprueba si la lista de dojos no está vacía */}
         {list ? (
           list.map((e, i) => (
+            // Enlace a los detalles del dojo
             <Link
               href={`/dojos/${e.member_dojo_id.dojo_category_id.category_id}`}
+              key={i} // Se recomienda agregar una clave única cuando se utiliza map
             >
+              {/* Tarjeta de dojo */}
               <div className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded cursor-pointer m-1">
+                {/* Nombre del dojo */}
                 <h3>{e.member_dojo_id.dojo_category_id.category_name}</h3>
               </div>
             </Link>
           ))
         ) : (
+          // Mensaje si no hay dojos en la lista
           <>No hay dojos</>
         )}
       </div>
