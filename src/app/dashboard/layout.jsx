1d47a204ceaf274fb4aa9652c91198b0d563638f
sircagev <cf.giron04@gmail.com>
2023-09-27 12:26:58 -0500

    update: add chart to dashboard

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,6 +2,8 @@ import Header from "@/components/dashboard/header"
 import LateralNav from "@/components/dashboard/navlateral";
 import TitleSection from "@/components/dashboard/titleSection";
 import CardItem from "@/components/dashboard/cardItem";
+import Chart from "@/components/dashboard/chart";
+
 
 export default function DashboardLayout() {
   return (
@@ -18,6 +20,12 @@ export default function DashboardLayout() {
                   <CardItem number="1000" modulo="Publicaciones"></CardItem>
                   <CardItem number="50" modulo="Dojos"></CardItem>
                   <CardItem number="5689" modulo="Comentarios"></CardItem>
+                </section>
+                <section className="grid md:grid-cols-2 xl:grid-cols-4 xl:grid-rows-3 xl:grid-flow-col gap-6">
+                  <Chart name="Publicaciones por dia" chart="Chart"></Chart>
+                  <div class="row-span-3 bg-white shadow rounded-lg">
+                    
+                  </div>
                 </section>
               </main>
             </div>  
