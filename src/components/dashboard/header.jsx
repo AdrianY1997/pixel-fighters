49a0ed7c24a84df0349cb350b289d13784fdbc5c
adriany1997 <yasnoadriandatos@gmail.com>
2023-09-27 19:28:25 -0500

    fix

--- src/components/dashboard/header.jsx
+++ src/components/dashboard/header.jsx
@@ -52,10 +52,7 @@ export default function Header({ user, role }) {
               />
             </svg>
           </button> */}
-          <button
-            onClick={() => signOut()}
-            className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full"
-          >
+          <button className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
             <span className="sr-only">Log out</span>
             <svg
               aria-hidden="true"
